4eb83c251a177d66b8fb975e07bdb35904b07f66
MiguelJDZ <71290776+MiguelJDZ@users.noreply.github.com>
2022-08-10 16:19:47 -0400

    Update Department.cs

--- CourseMaintenanceWinFormsApp/Domain/Department.cs
+++ CourseMaintenanceWinFormsApp/Domain/Department.cs
@@ -1,11 +1,3 @@
-﻿/*
- * File: Department.cs
- * Author: Miguel Duran
- * Course: COTI 4150-KJ1 Prof. Antonio F. Huertas
- * Date: 05/15/2021
- * Purpose: This class represent a department.
- */
-
 namespace CourseMaintenanceWinFormsApp.Domain
 {
     /// <summary>
