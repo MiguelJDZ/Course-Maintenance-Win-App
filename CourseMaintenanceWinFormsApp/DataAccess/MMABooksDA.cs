12de891413b9856ea3a8dd1724a87973074fe975
MiguelJDZ <71290776+MiguelJDZ@users.noreply.github.com>
2022-08-10 16:20:53 -0400

    Update MMABooksDA.cs

--- CourseMaintenanceWinFormsApp/DataAccess/MMABooksDA.cs
+++ CourseMaintenanceWinFormsApp/DataAccess/MMABooksDA.cs
@@ -1,12 +1,3 @@
-﻿/*
- * File: MMABooksDA.cs
- * Author: Antonio F. Huertas 841-##-####
- * Course: COTI 4150-KJ1 Prof. Antonio F. Huertas
- * Date: 05/15/2021
- * Purpose: This class provides data access for the MMABooks database.
- * NOTE: The Data Directory is the folder that contains the application's executable file.
- */
-
 using System.Configuration;
 
 namespace CourseMaintenanceWinFormsApp.DataAccess
